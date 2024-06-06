e748812844f05a30ec05ed22df20881bfb6e580d
Dhanish Ahamed <danishahamed.dk4@gmail.com>
2024-06-06 14:48:19 +0530

    sample commit

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    return(
+        <div>helo</div>
+    )
 };
 
 export default CartValue;
